8d879080204605c789bd23ba0c86f93a18a4325d
Rupesh Kumar Dash <rupeshkumardash123@gmail.com>
2021-05-27 18:10:51 +0530

    third day commit

--- activity/script.js
+++ activity/script.js
@@ -1,12 +1,20 @@
 let videoElem = document.querySelector("video");
 // 1. 
 let recordBtn = document.querySelector(".record");
+let captureImgBtn = document.querySelector(".click-image")
+let filterArr = document.querySelectorAll(".filter");
+let filterOverlay = document.querySelector(".filter_overlay");
+let timings = document.querySelector(".timing");
 let isRecording = false;
+let filterColor = "";
+let counter = 0;
+let clearObj;
+
 // user  requirement send 
 let constraint = {
     audio: true, video: true
 }
-// represent future recoding
+// represent future recording
 let recording = [];
 let mediarecordingObjectForCurrStream;
 // promise 
@@ -21,12 +29,14 @@ usermediaPromise.
         mediarecordingObjectForCurrStream = new MediaRecorder(stream);
         // camera recording add -> recording array
         mediarecordingObjectForCurrStream.ondataavailable = function (e) {
+           
             recording.push(e.data);
         }
         // download
         mediarecordingObjectForCurrStream.addEventListener("stop", function () {
             // recording -> url convert 
             // type -> MIME type (extension)
+            
             const blob = new Blob(recording, { type: 'video/mp4' });
             const url = window.URL.createObjectURL(blob);
             let a = document.createElement("a");
@@ -48,10 +58,59 @@ recordBtn.addEventListener("click", function () {
     if (isRecording == false) {
         mediarecordingObjectForCurrStream.start();
         recordBtn.innerText = "Recording....";
+        startTimer();
     }
     else {
+        stopTimer();
         mediarecordingObjectForCurrStream.stop();
         recordBtn.innerText = "Record";
     }
     isRecording = !isRecording
 })
+captureImgBtn.addEventListener("click", function () {
+    // canvas create 
+    let canvas = document.createElement("canvas");
+    canvas.height = videoElem.videoHeight;
+    canvas.width = videoElem.videoWidth;
+    let tool = canvas.getContext("2d");
+    tool.drawImage(videoElem, 0, 0);
+    if (filterColor) {
+        tool.fillStyle = filterColor;
+        tool.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    let url = canvas.toDataURL();
+    let a = document.createElement("a");
+    a.download = "file.png";
+    a.href = url;
+    a.click();
+    a.remove();
+    // videoELement
+})
+// filter Array
+for (let i = 0; i < filterArr.length; i++) {
+    filterArr[i].addEventListener("click", function () {
+        filterColor = filterArr[i].style.backgroundColor;
+        filterOverlay.style.backgroundColor = filterColor;
+    })
+}
+
+function startTimer() {
+    timings.style.display = "block";
+    function fn() {
+        // hours
+        let hours = Number.parseInt(counter / 3600);
+        let RemSeconds = counter % 3600;
+        let mins = Number.parseInt(RemSeconds / 60);
+        let seconds = RemSeconds % 60;
+        hours = hours < 10 ? `0${hours}` : hours;
+        mins = mins < 10 ? `0${mins}` : `${mins}`;
+        seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+        timings.innerText = `${hours}:${mins}:${seconds}`
+        counter++;
+    }
+    clearObj = setInterval(fn, 1000);
+}
+function stopTimer() {
+    timings.style.display = "none";
+    clearInterval(clearObj);
+}
